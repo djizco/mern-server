08a44cb68c3c2e50c1dfc3f7cf26bb5ae647f59e
Rico Rojas <djizco@yahoo.com>
2021-04-06 22:48:22 -0500

    Add CORS configuration

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express      = require('express');
 const bodyParser   = require('body-parser');
+const cors         = require('cors');
 
 require('./environment');
 require('./database');
@@ -8,9 +9,14 @@ const routes          = require('./routes');
 const configPassport  = require('./passport/passport-config');
 
 const port         = process.env.PORT;
+const origin       = process.env.ORIGIN;
 const app          = express();
 
 app.use(bodyParser.json());
+app.use(cors({
+  origin,
+  optionsSuccessStatus: 200,
+}))
 
 configPassport(app, express);
 
